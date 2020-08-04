92b4296d694c4b250072821b05a966eaf554cd60
yashwagle <as>
2020-08-04 18:08:20 -0400

    add frontend

--- FlaskBackend/MemeGenerator.py
+++ FlaskBackend/MemeGenerator.py
@@ -1,14 +1,27 @@
 from flask import Flask
-from urllib.request import urlopen
-import json
-
+from flask_cors import CORS
+import requests
 
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def downloadImage():
-    return "Hello World"
+images = ""
+
+@app.route('/getimages')
+def sendImageURLS():
+    global images
+    return images.text
+
+
+
+
+def getImages():
+    global images
+    images = requests.get("https://api.imgflip.com/get_memes")
+
 
 
 if __name__ == '__main__':
+    getImages()
+    app.run(debug=True)
